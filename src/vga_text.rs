57770cc65b5885fd1763c551bd1ee1cfae68a831
omaremadcc <om3x4e@gmail.com>
2026-05-09 21:37:38 +0300

    Improve VGA text writer and implement scrolling
    Add support for newlines, tabs, and automatic scrolling to the VGA
    text buffer. Use volatile operations for memory access and switch to
    lazy_static for the global writer instance.

--- src/vga_text.rs
+++ src/vga_text.rs
@@ -28,16 +28,18 @@ enum CharacterColor {
 }
 
 pub fn write() {
-    // println("Omar EmadoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooEmadoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooEmadooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo");
-    // println("Omar Emad");
-    // println("mohamed");
+    println(
+        "Omar EmadoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooEmadoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooEmadooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo",
+    );
+    println("Omar Emad");
+    println("mohamed");
     println("khal");
 }
-
 
 pub fn println(text: &str) {
     let mut writer = WRITER.lock();
 
+    writer.write_byte(b'\n');
     for byte in text.bytes() {
         writer.write_byte(byte);
     }
@@ -46,48 +48,85 @@ pub fn println(text: &str) {
 const BUFFER_WIDTH: usize = 80;
 const BUFFER_HEIGHT: usize = 25;
 
+#[repr(transparent)]
 struct Buffer {
-    chars: [[core::ptr::NonNull<Character>; BUFFER_WIDTH]; BUFFER_HEIGHT]
+    chars: [[Character; BUFFER_WIDTH]; BUFFER_HEIGHT],
 }
 
 impl Buffer {
     pub fn new_line(&mut self) {
         for row in 1..BUFFER_HEIGHT {
             for col in 0..BUFFER_WIDTH {
-                let character = self.chars[row][col];
-                self.chars[row - 1][col] = character;
+                // Inside Buffer::new_line
+                let character =
+                    unsafe { core::ptr::read_volatile(&self.chars[row][col] as *const Character) };
+                unsafe {
+                    core::ptr::write_volatile(
+                        &mut self.chars[row - 1][col] as *mut Character,
+                        character,
+                    );
+                }
+            }
+        }
+        for col in 0..BUFFER_WIDTH {
+            let char = Character {
+                char: b' ',
+                color: CharacterColor::White,
+            };
+            unsafe {
+                core::ptr::write_volatile(
+                    &mut self.chars[BUFFER_HEIGHT - 1][col] as *mut Character,
+                    char,
+                );
+            };
+        }
+    }
+}
+
+unsafe impl Send for Writer {}
+unsafe impl Sync for Writer {}
+
+pub struct Writer {
+    column_position: usize,
+    buffer: &'static mut Buffer,
+}
+
+impl Writer {
+    pub fn write_byte(&mut self, byte: u8) {
+        match byte {
+            b'\n' => {
+                self.column_position = 0;
+                self.buffer.new_line();
+            }
+            b'\t' => {
+                self.column_position += 4;
+                if self.column_position >= BUFFER_WIDTH {
+                    self.column_position = 0;
+                    self.buffer.new_line();
+                }
+            }
+            _ => {
+                (*self.buffer).chars[BUFFER_HEIGHT - 1][self.column_position] = Character {
+                    char: byte,
+                    color: CharacterColor::White,
+                };
+                if self.column_position == BUFFER_WIDTH - 1 {
+                    self.buffer.new_line();
+                    self.column_position = 0;
+                } else {
+                    self.column_position += 1;
+                }
             }
         }
     }
 }
 
-pub struct Writer {
-    column_position: usize,
-    buffer: *mut Buffer,
-}
-
-impl Writer {
-    pub fn write_byte(&mut self, byte: u8) {
-        let buffer = self.buffer;
-        unsafe {
-            (*buffer).chars[0][0].write_volatile(Character {
-                char: b'O',
-                color: CharacterColor::White,
-            });
-            self.column_position += 1;
-        }
-    }
-}
-
-// Promise that Writer can be sent to other threads
-unsafe impl Send for Writer {}
-
-// Promise that Writer can be shared between threads (needed for static Mutex)
-unsafe impl Sync for Writer {}
-
+use lazy_static::lazy_static;
 use spin::Mutex;
 
-pub static WRITER: Mutex<Writer> = Mutex::new(Writer {
-    column_position: 0,
-    buffer: 0xb8000 as *mut Buffer,
-});
+lazy_static! {
+    pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
+        column_position: 0,
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+    });
+}
